85a0dbe88e98f9fdcd584a918efe3518edbca0a1
Ada0910 <1355948107@qq.com>
2022-01-05 23:42:25 +0800

    合并两个有序链表

---
+++ [21]合并两个有序链表.java
@@ -0,0 +1,17 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
