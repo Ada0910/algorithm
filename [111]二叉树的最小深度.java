9ccea0c0a6046f6cea8020a0ec41276519f53799
xieweining <1355948107@qq.com>
2022-07-05 15:52:39 +0800

    [111]二叉树的最小深度

---
+++ [111]二叉树的最小深度.java
@@ -0,0 +1,47 @@
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+	public int minDepth(TreeNode root) {
+		//树为空
+		if (root == null) {
+			return 0;
+		}
+
+		//左节点和右节点都为空
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+
+		int min_depth = Integer.MAX_VALUE;
+
+		if (root.left != null) {
+			min_depth = Math.min(minDepth(root.left), min_depth);
+		}
+
+		if (root.right != null) {
+			min_depth = Math.min(minDepth(root.right), min_depth);
+		}
+
+		return min_depth + 1;
+	}
+
+	
+}
+//leetcode submit region end(Prohibit modification and deletion)
