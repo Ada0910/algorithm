5f5ebd0ea4126fa2aedf8e1b6040ba0276f753af
xieweining <1355948107@qq.com>
2023-03-22 18:14:22 +0800

    304矩阵不可变

--- [304]二维区域和检索 - 矩阵不可变.java
+++ [304]二维区域和检索 - 矩阵不可变.java
@@ -1,36 +1,56 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class NumMatrix {
+//class NumMatrix {
 
-    //维护一个二维数组
-    int[][] preSum;
-    
-    public NumMatrix(int[][] matrix) {
-        //初始化
-        //行数
-        int m = matrix.length;
-        //列数
-        int n = matrix[0].length;
-        
-        //base case
-        if(m == 0|| n==0){
-            return ;
-        }
-        
-        preSum = new int[m+1][n+1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                //preSum记录的是以ij为原点的值
-                preSum[i][j] =preSum[i-1][j]+preSum[i][j-1] -preSum[i-1][j-1] + matrix[i-1][j-1];
-            }
-            
-        }
-    }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        return preSum[row2+1][col2+1] -preSum[row1+1][col2]-preSum[row2][col1+1]+preSum[row1][col1];
-    }
+////维护一个二维数组
+//int[][] preSum;
+//
+//public NumMatrix(int[][] matrix) {
+//    //初始化
+//    //行数
+//    int m = matrix.length;
+//    //列数
+//    int n = matrix[0].length;
+//    
+//    //base case
+//    if(m == 0|| n==0){
+//        return ;
+//    }
+//    
+//    preSum = new int[m+1][n+1];
+//    for (int i = 0; i < m; i++) {
+//        for (int j = 0; j < n; j++) {
+//            //preSum记录的是以ij为原点的值
+//            preSum[i+1][j+1] =preSum[i+1][j]+preSum[i][j+1] -preSum[i][j] + matrix[i][j];
+//        }
+//        
+//    }
+//}
+//
+//public int sumRegion(int row1, int col1, int row2, int col2) {
+//    return preSum[row2+1][col2+1] -preSum[row1+1][col2]-preSum[row2][col1+1]+preSum[row1][col1];
+//}
+
+//}
+
+class NumMatrix {
+	int[][] sums;
+
+	public NumMatrix(int[][] matrix) {
+		int m = matrix.length;
+		if (m > 0) {
+			int n = matrix[0].length;
+			sums = new int[m + 1][n + 1];
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+				}
+			}
+		}
+	}
+
+	public int sumRegion(int row1, int col1, int row2, int col2) {return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];}
 }
 
 /**
