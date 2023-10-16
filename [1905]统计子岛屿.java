ad6449433fc39772ee36403f3241fc9d6856f1b6
Ada0910 <1355948107@qq.com>
2023-10-17 00:04:39 +0800

    统计子岛屿

---
+++ [1905]统计子岛屿.java
@@ -0,0 +1,45 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length, n = grid1[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid1[i][j] == 0 && grid2[i][j] == 1) {
+// 这个岛屿肯定不是⼦岛，淹掉
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+
+        /// 现在 grid2 中剩下的岛屿都是⼦岛，计算岛屿数量
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    res++;
+                    dfs(grid2, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+
+    // 从 (i, j) 开始，将与之相邻的陆地都变成海⽔
+    void dfs(int[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
+        }
+        if (grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
